fef9e3054f67541564d0d07abb774a3c1f4763ff
codystack <ebuzzadvert@gmail.com>
2025-11-16 16:46:29 +0100

    Executive Coaching added

---
+++ executive-coaching.php
@@ -0,0 +1,161 @@
+<?php
+include "./components/header.php";
+include "./components/offcanvas.php";
+include "./components/navbar-alt.php";
+?>
+    <div class="breadcrumb-banner section-bg position-relative">
+        <div class="container pe-0 ps-0">
+            <img src="assets/img/breadcrumb-about.png" alt="img" class="w-100">
+            <h1 class="breadcrumb-title-services">Executive Coaching</h1>
+            <div class="boxes">
+                <ul class="breadcrumb-cont d-flex flex-wrap ps-1 align-items-center gap-md-3 gap-2" style="list-style: none;">
+                    <li class="theme-clr4 fw-500">
+                        <a href="./" class="fw-600">Home</a>
+                    </li>
+                    <li>
+                        <i class="fa-solid fa-arrow-right"></i>
+                    </li>
+                    <li class="fz-16 theme-clr fw-500">
+                        Executive coaching
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+
+    <section class="whatdow-section section-bg pt-100 pb-100">
+        <div class="container">
+            <div class="row g-lg-4 g-md-3 g-2 align-items-lg-end mb-40">
+                <div class="col-lg-7 col-md-7">
+                    <div class="section-header">
+                        <h2 class="theme-clr4 fw-bold wow fadeInUp mb-2" data-wow-delay=".3s">
+                            Don't go through life,<br />
+                            <span class="fw-300">grow through life.<br></span>
+                        </h2>
+                        <span class="fz-20 text-danger fw-300">- Eric Butterworth</span>
+                    </div>
+                </div>
+                <div class="col-lg-5 col-md-5">
+                    <div class="wow fadeInUp" data-wow-delay=".4s">
+                        <p class="theme-clr4 mb-lg-4 mb-3">
+                        As an experienced executive coach, I partner with high-performing leaders to refine their strategic vision, elevate their leadership presence, and drive measurable impact. My approach combines global corporate insight (from McKinsey & Rand Merchant Bank) with entrepreneurial wisdom—helping you bridge strategy with execution.
+                        </p>
+                        
+                    </div>
+                </div>
+            </div>
+            <div class="row g-xxl-5 g-lg-4 align-items-center g-md-3 g-3">
+                <div class="col-lg-6">
+                    <img src="assets/img/ec-img1.jpg" alt="img" class="w-100 rounded-4">
+                </div>
+                <div class="col-lg-6">
+                    <div class="wow fadeInUp" data-wow-delay=".4s">
+                        <h3 class="mb-3">One Hour Clarity Session</h3>
+                        <p class="theme-clr4 mb-lg-3 mb-2">
+                            Feeling stuck, overwhelmed, or unsure of your next professional move? Or maybe you’re curious about coaching and want to experience it before committing?
+                        </p>
+                        <p class="theme-clr4 mb-lg-3 mb-2">This 1-hour clarity session is designed to help you cut through the noise, understand your options, and move forward with confidence.</p>
+                        <p class="theme-clr4 mb-lg-3 mb-2">Whether you want to solve a specific challenge or gain direction for your next step, this session gives you focused insight and actionable guidance.</p>
+                        <p class="theme-clr4 mb-lg-3 mb-2">After booking, you’ll receive immediate access to my calendar to schedule your session at a convenient time.</p>
+                        <a href="get-started" class="theme-btn style1 pe-20">
+                            <i class="fa-solid fa-arrow-right w-36 h-36 bg-white rounded-circle d-center fz-14 theme-clr4"></i>
+                            Book a Session
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="whatdow-section pt-100 pb-100">
+        <div class="container">
+            <div class="row g-xxl-5 g-lg-4 align-items-center g-md-3 g-3">
+                <div class="col-lg-6">
+                    <div class="wow fadeInUp" data-wow-delay=".4s">
+                        <h3 class="mb-3">Take Control CareerMaker Coaching Program</h3>
+                        <p class="theme-clr4 mb-lg-3 mb-2">The <b>Take Control CareerMaker Program</b> is a 1-on-1, 7-week coaching experience designed for mid-level managers and executives who are ready to take ownership of their career path and accelerate their growth.</p>
+                        <p>
+                            This program is perfect for you if:
+                            <ul class="ps-4">
+                                <li>You feel you’re capable of more but aren’t sure what the next level looks like.</li>
+                                <li>You’ve been in the same role for a while and feel stuck or uncertain about your next move.</li>
+                                <li>You believe you should be advancing faster, but promotions aren’t happening—and you know you’re not fully steering your career.</li>
+                            </ul>
+                        </p>
+                        <b>What’s Included:</b>
+                        <p>
+                            This program is perfect for you if:
+                            <ul class="ps-4">
+                                <li>7 private coaching sessions (45 minutes each)</li>
+                                <li>Unlimited email access throughout the program</li>
+                                <li>WhatsApp support during business hours for real-time clarity and accountability</li>
+                            </ul>
+                        </p>
+                        <p class="theme-clr4 mb-lg-3 mt-3">The <b>Take Control CareerMaker Program</b> is built for executives and high-potential managers who want a structured, professional, and results-driven approach to career advancement.</p>
+                        <a href="get-started" class="theme-btn style1 pe-20">
+                            <i class="fa-solid fa-arrow-right w-36 h-36 bg-white rounded-circle d-center fz-14 theme-clr4"></i>
+                            Take Control Now
+                        </a>
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <img src="assets/img/ec-img2.jpg" alt="img" class="w-100 rounded-4">
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="whatdow-section section-bg pt-100 pb-100">
+        <div class="container">
+            <div class="row g-xxl-5 g-lg-4 align-items-center g-md-3 g-3">
+                <div class="col-lg-6">
+                    <img src="assets/img/ec-img3.jpg" alt="img" class="w-100 rounded-4">
+                </div>
+                <div class="col-lg-6">
+                    <div class="wow fadeInUp" data-wow-delay=".4s">
+                        <h3 class="mb-3">BICEP Coaching Program (3–6 Months)</h3>
+                        <p class="theme-clr4 mb-lg-3 mb-2">Imagine taking full control of your leadership and career trajectory—growing at a pace that matches your ambition. The BICEP Coaching Program is an intensive 1:1 executive coaching experience designed to help you elevate your performance, strengthen your leadership, and confidently drive your growth journey.</p>
+                        <p>
+                            This program is ideal for you if:
+                            <ul class="ps-4">
+                                <li>You want to accelerate your performance and deliver greater impact in your current role.</li>
+                                <li>You’ve recently been promoted and want to excel in your first 90 days.</li>
+                                <li>You aspire to a higher leadership position and need a clear, strategic path to get there.</li>
+                            </ul>
+                        </p>
+                        <b class="mt-3">Program Duration:</b>
+                        <p class="theme-clr4 mb-lg-3 mb-2">A tailored 3- or 6-month executive coaching program built around your goals, pace, and growth ambitions.</p>
+                        <a href="get-started" class="theme-btn style1 pe-20">
+                            <i class="fa-solid fa-arrow-right w-36 h-36 bg-white rounded-circle d-center fz-14 theme-clr4"></i>
+                            Book a Consultation.
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="whatdow-section pt-100 pb-100">
+        <div class="container">
+            <div class="row g-xxl-5 g-lg-4 align-items-center g-md-3 g-3">
+                <div class="col-lg-6">
+                    <div class="wow fadeInUp" data-wow-delay=".4s">
+                        <h3 class="mb-3">VIP Strategy Day</h3>
+                        <p class="theme-clr4 mb-lg-3 mb-2">The VIP Strategy Day is a fully immersive, high-impact experience designed entirely around you. Its purpose is simple: to create a breakthrough and propel you forward on your career journey—fast.</p>
+                        <p class="theme-clr4 mb-lg-3 mb-2">Across 7–9 hours of intensive, focused, and strategic coaching, we’ll dive deep into visioning, planning, and performance transformation. You get exclusive, one-on-one access to my full expertise as a coach, consultant, and mentor.</p>
+                        <p class="theme-clr4 mb-lg-3 mb-2">If you want accelerated growth, clarity, and hands-on support to rapidly elevate your career, the VIP Strategy Day is your fast track.</p>
+                        <a href="get-started" class="theme-btn style1 pe-20">
+                            <i class="fa-solid fa-arrow-right w-36 h-36 bg-white rounded-circle d-center fz-14 theme-clr4"></i>
+                            Claim Your VIP Session
+                        </a>
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <img src="assets/img/ec-img4.jpg" alt="img" class="w-100 rounded-4">
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+<?php include "./components/footer.php"; ?>
